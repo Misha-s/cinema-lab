c7d10a3dd26ffd05634764a9d3ed441c6222222d
Mike <miksid@protonmail.com>
2023-11-12 21:35:43 -0500

    Initial commit

---
+++ src/test/java/com/tomato/cinemalab/CinemaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.cinemalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
